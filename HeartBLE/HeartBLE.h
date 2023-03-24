02092c7874cef503bb47b38c328708e009dcf2d9
Carlos Murillo <93104423+Tiz52@users.noreply.github.com>
2023-03-24 01:53:28 -0500

    Initial Commit

---
+++ HeartBLE/HeartBLE.h
@@ -0,0 +1,18 @@
+//
+//  HeartBLE.h
+//  HeartBLE
+//
+//  Created by Carlos Murillo on 24/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HeartBLE.
+FOUNDATION_EXPORT double HeartBLEVersionNumber;
+
+//! Project version string for HeartBLE.
+FOUNDATION_EXPORT const unsigned char HeartBLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeartBLE/PublicHeader.h>
+
+
